d671b25d2f02d3529c5d0d0bc7844b0d8ee6d151
kdurant_work <wangjun850725@163.com>
2019-05-10 17:02:11 +0800

    sync

---
+++ c/tree.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+
+
+void init_tree(struct TreeNode * tree)
+{
+}
+
+bool tree_is_empty(struct TreeNode **tree)
+{
+    return false;
+}
+
+int tree_depth(struct TreeNode **tree)
+{
+    return 0;
+}
+
+int find_node(struct TreeNode **tree, int data)
+{
+    return 0;
+}
+
+void pre_order(struct TreeNode **tree)
+{
+
+}
+
+void mid_order(struct TreeNode ** tree)
+{
+
+}
+
+void post_order(struct TreeNode ** tree)
+{
+    
+}
+
+int main()
+{
+    struct TreeNode * tree;
+    
+}
