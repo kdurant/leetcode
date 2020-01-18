3be7d2fc7b961b89be75fed730ca75f85460d709
kdurant <wangjun850725@163.com>
2020-01-18 19:01:22 +0800

    add 1160 countCharacters.cpp

---
+++ cpp/_1160 countCharacters.cpp
@@ -0,0 +1,76 @@
+/*
+给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
+
+假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），那么我们就认为你掌握了这个单词。
+
+注意：每次拼写时，chars 中的每个字母都只能用一次。
+
+返回词汇表 words 中你掌握的所有单词的 长度之和。
+
+ 
+
+示例 1：
+
+输入：words = ["cat","bt","hat","tree"], chars = "atach"
+输出：6
+解释：
+可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
+示例 2：
+
+输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+输出：10
+解释：
+可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-words-that-can-be-formed-by-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int countCharacters(vector<string>& words, string chars)
+    {
+        int            ret = 0;
+        map<char, int> dict, t;
+        for(auto i : chars)
+            dict[i]++;
+
+        for(auto i : words)
+        {
+            int flag = 1;
+            t        = dict;
+            for(auto j : i)
+            {
+                t[j]--;
+                if(t[j] < 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag)
+                ret += i.size();
+        }
+        return ret;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    vector<string> words = {"hello", "world", "leetcode"};
+    string         chars = "welldonehoneyr";
+    Solution       s;
+    cout << s.countCharacters(words, chars) << endl;
+    return 0;
+}
