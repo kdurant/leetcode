414801ab50566c78260ba631d478fcb092137cbc
kdurant <wangjun850725@163.com>
2020-01-23 20:58:27 +0800

    add subset1() in 0078. 位运算效率远超回溯

--- cpp/0078 subsets.cpp
+++ cpp/0078 subsets.cpp
@@ -1,9 +1,34 @@
+/*
+给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+
+说明：解集不能包含重复的子集。
+
+示例:
+
+输入: nums = [1,2,3]
+输出:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/subsets
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 #include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <map>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -29,6 +54,29 @@ public:
         vector<int>         item;
         res.push_back(item);
         generate(0, nums, item, res);
+        return res;
+    }
+
+    vector<vector<int>> subsets1(vector<int>& nums)
+    {
+        vector<vector<int>> res;
+        vector<int>         item;
+
+        int size = pow(2, nums.size());
+
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < nums.size(); j++)
+            {
+                if((i & (1 << j)) & (1 << j))
+                {
+                    item.push_back(nums[j]);
+                }
+            }
+            res.push_back(item);
+            item.clear();
+        }
+
         return res;
     }
 };
