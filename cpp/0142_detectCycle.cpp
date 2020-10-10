68444cc5ff6d70d005c60c01932b10ffe226acc9
kdurant <wangjun850725@163.com>
2020-10-10 09:10:00 +0800

    add 0142_detectCycle.cpp

---
+++ cpp/0142_detectCycle.cpp
@@ -0,0 +1,44 @@
+/*
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+说明：不允许修改给定的链表。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct ListNode
+{
+    int       val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        unordered_set<ListNode *> visted;
+
+        while(head != nullptr)
+        {
+            if(visted.count(head))
+                return head;
+
+            visted.insert(head);
+            head = head->next;
+        }
+        return nullptr;
+    }
+};
